6ae75ca996d08f12344d6a03a81d79f7eef0288f
usm4nq <usm4nq@gmail.com>
2019-03-29 02:47:43 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,127 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const url = require("url");
+const template = [
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click() { require('electron').shell.openExternal('https://electronjs.org') }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    })
+
+    // Edit menu
+    template[1].submenu.push(
+        { type: 'separator' },
+        {
+            label: 'Speech',
+            submenu: [
+                { role: 'startspeaking' },
+                { role: 'stopspeaking' }
+            ]
+        }
+    )
+
+    // Window menu
+    template[3].submenu = [
+        { role: 'close' },
+        { role: 'minimize' },
+        { role: 'zoom' },
+        { type: 'separator' },
+        { role: 'front' }
+    ]
+}
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    // load the dist folder from Angular
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/index.html`),
+            protocol: "file:",
+            slashes: true
+        })
+    );
+
+    // The following is optional and will open the DevTools:
+    // win.webContents.openDevTools()
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
+import { MatInputModule, MatTableModule } from '@angular/material';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'Reports', component: ReportsComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: ReportsComponent }//page not found
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReportsComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
