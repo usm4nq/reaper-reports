8409be09250081f4aeebd734db2743e72a590113
usm4nq <usm4nq@gmail.com>
2019-03-29 13:39:53 -0700

    create report update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { ReportsComponent } from './reports/reports.component';
-import { MatInputModule, MatTableModule, MatCardModule, MatGridListModule, MatButtonModule, MatSelectModule, MatFormFieldModule, MatDividerModule } from '@angular/material';
+import { MatInputModule, MatTableModule, MatCardModule, MatGridListModule, MatButtonModule, MatSelectModule, MatFormFieldModule, MatDividerModule, MatListModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { CreateReportComponent } from './create-report/create-report.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'Reports', component: ReportsComponent },
-  { path: 'create-new-report', component: CreateReportComponent },
+  { path: 'Create-New-Report', component: CreateReportComponent},
   {
     path: '',
-    redirectTo: '/create-new-report',
+    redirectTo: '/Create-New-Report',
     pathMatch: 'full'
   },
-  { path: '**', component: CreateReportComponent }//page not found
+  { path: '**', component: HomeComponent }//page not found
 ]
 
 @NgModule({
@@ -35,6 +36,7 @@ const appRoutes: Routes = [
     ),
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatInputModule,
     MatTableModule,
     MatCardModule,
@@ -43,6 +45,7 @@ const appRoutes: Routes = [
     MatSelectModule,
     MatFormFieldModule,
     MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-report/create-report.component.ts
+++ src/app/create-report/create-report.component.ts
@@ -1,21 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface ComponentList {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-create-report',
   templateUrl: './create-report.component.html',
   styleUrls: ['./create-report.component.css']
 })
 export class CreateReportComponent implements OnInit {
-  components: ComponentList[] = [
-    {value: 'header', viewValue: 'Header'},
-    {value: 'table', viewValue: 'Table'},
-    {value: 'footer', viewValue: 'Footer'},
-    {value: 'divider', viewValue: 'Divider'}
+
+  report: any = [
+    {
+      cType: "table",
+      rows:
+        [
+          {
+            data:
+              [
+                { cType: "label", text: "Name :" },
+                { cType: "label", text: "Noman" }
+              ]
+          },
+          {
+            data:
+              [
+                { cType: "label", text: "Age :"},
+                { cType: "label", text: 23}
+              ]
+          }
+        ]
+    },
+    {
+      cType: "table",
+      rows:
+      [
+        {
+          data:
+          [
+            {cType: "label", text: ""},
+            {cType: "label", text: "Paper"},
+            {cType: "label", text: "Marks"}
+          ]
+        },
+        {
+          data:
+          [
+            {cType: "label", text: 1},
+            {cType: "label", text: "Maths"},
+            {cType: "label", text: 100}
+          ]
+        },
+        {
+          data:
+          [
+            {cType: "label", text: 2},
+            {cType: "label", text: "Physics"},
+            {cType: "label", text: 97}
+          ]
+        }
+      ]
+    }
   ];
   constructor() { }
 
