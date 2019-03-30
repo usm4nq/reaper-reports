d3fa2d0242ea56919a7a9015f05b1eaa3ba66ebc
usm4nq <usm4nq@gmail.com>
2019-03-30 04:58:16 -0700

    minor changes

--- src/app/create-report/create-report.component.ts
+++ src/app/create-report/create-report.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 abstract class ReaperComponent {
   
 }
-class ReaperTable extends ReaperComponent {
+class ReaperSection extends ReaperComponent { 
+  name: String;
   cType: String;
   rows: any[];
-  constructor(rows: any[]) {
+  constructor(rows: any[], name: String) {
     super();
+    this.name = name;
     this.cType = "table";
     this.rows = rows
   }
 }
 
-class ReaperTableRow extends ReaperComponent {
+class ReaperRow extends ReaperComponent {
   data: ReaperComponent[];
 
   constructor(data: ReaperComponent[]) {
@@ -37,53 +39,77 @@ class ReaperLabel {
 })
 export class CreateReportComponent implements OnInit {
 
+  reports: String[] = ["aaa","bbb","ccc"];
+
   report: any = {
 
     template: [
-    new ReaperTable([
-      new ReaperTableRow(
+    new ReaperSection([
+      new ReaperRow(
         [
           new ReaperLabel("Name :"),
           new ReaperLabel("Noman")
         ]
       ),
-      new ReaperTableRow(
+      new ReaperRow(
         [
           new ReaperLabel("Age :"),
           new ReaperLabel(13)
         ]
       )
-    ]),
-    new ReaperTable(
+    ], "Header"),
+    new ReaperSection(
       [
-        new ReaperTableRow(
+        new ReaperRow(
           [
             new ReaperLabel(""),
             new ReaperLabel("Paper"),
             new ReaperLabel("Marks")
           ]
         ),
-        new ReaperTableRow(
+        new ReaperRow(
           [
             new ReaperLabel(1),
             new ReaperLabel("Maths"),
             new ReaperLabel(100)
           ]
         ),
-        new ReaperTableRow(
+        new ReaperRow(
           [
             new ReaperLabel(2),
             new ReaperLabel("Physics"),
             new ReaperLabel(97)
           ]
         )
-      ]
+      ],
+      "Body"
     )
   ]};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewSection(name: String) {
+    this.report.template.push(
+      new ReaperSection([
+        new ReaperRow(
+          [
+            new ReaperLabel("Name :"),
+            new ReaperLabel("Noman")
+          ]
+        ),
+        new ReaperRow(
+          [
+            new ReaperLabel("Age :"),
+            new ReaperLabel(13)
+          ]
+        )
+      ],
+      name
+      )
+    )
   }
 
 }
